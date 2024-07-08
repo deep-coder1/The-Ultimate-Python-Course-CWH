28d7f7c32ef4f11b35ecfa47effe9de0ecc2ffb0
deep-coder1 <ndktbd@gmail.com>
2024-07-08 18:56:37 +0530

    First Chapter 1, Comments Single line and Multi line.

--- Chapter 1/module.py
+++ Chapter 1/module.py
@@ -1,7 +1,22 @@
 import pyjokes
 
+# Single-Line Comments
+# print("Printing Jokes")
+
+# This prints a random jokes
 joke = pyjokes.get_joke()
 print(joke)
+
+# Multiline Comments
+""""
+so thanks
+that was my program
+"""
+
+# so thanks
+# that was my program
+# another line
+# yet another line
 
 # O/P:
 # python .\module.py
