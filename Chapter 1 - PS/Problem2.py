57a3657a69fd2112e5ebb8b8e6755635a2eb7076
deep-coder1 <ndktbd@gmail.com>
2024-07-09 06:52:36 +0530

    Chapter 1 - PS, Problem1, Problem2 Solved.

---
+++ Chapter 1 - PS/Problem2.py
@@ -0,0 +1,28 @@
+# 2. Use REPL and print the table of 5 using it. 
+
+# Sol:
+
+# C:\Users\deepa>python
+# Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> 5*1
+# 5
+# >>> 5*2
+# 10
+# >>> 5*3
+# 15
+# >>> 5*4
+# 20
+# >>> 5*5
+# 25
+# >>> 5*6
+# 30
+# >>> 5*7
+# 35
+# >>> 5*8
+# 40
+# >>> 5*9
+# 45
+# >>> 5*10
+# 50
+# >>>
