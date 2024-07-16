e4fccf21b9d101ec7e49d583fc058975b0a3d466
deep-coder1 <ndktbd@gmail.com>
2024-07-16 18:02:46 +0530

    Comparison Operators, Chapter 2

--- Chapter 2/04_operators.py
+++ Chapter 2/04_operators.py
@@ -11,3 +11,9 @@ b = 6
 # b += 3 # Increment the value of b by 3 and then assign it to b
 b -= 3 # Decrement the value of b by 3 and then assign it to b
 print(b)
+
+# Comparison Operators
+
+d = 5==5
+
+print(d)
