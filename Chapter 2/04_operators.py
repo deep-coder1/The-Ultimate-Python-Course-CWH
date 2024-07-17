6f7a0b21c76dfad8231e6e62d6b050d8b7915309
deep-coder1 <ndktbd@gmail.com>
2024-07-17 15:50:11 +0530

    Logical Operators and Truth table of 'or' , Chapter 2

--- Chapter 2/04_operators.py
+++ Chapter 2/04_operators.py
@@ -17,3 +17,13 @@ print(b)
 d = 5==5
 
 print(d)
+
+# Logical Operators
+
+e = True or False
+
+# Truth table of 'or' 
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
