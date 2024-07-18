b9e761a7bec3ab45b5fb21ae88ec4ae0cb60cc1f
deep-coder1 <ndktbd@gmail.com>
2024-07-18 17:48:59 +0530

    Truth table of 'and' , Chapter 2

--- Chapter 2/04_operators.py
+++ Chapter 2/04_operators.py
@@ -27,3 +27,11 @@ print("True or False is ", True or False)
 print("True or True is ", True or True)
 print("False or True is ", False or True)
 print("False or False is ", False or False)
+
+# Truth table of 'and' 
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print(not(True))
