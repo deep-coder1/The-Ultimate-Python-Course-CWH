c92395ce5b95c777465c079493f8fcde483fd11d
deep-coder1 <ndktbd@gmail.com>
2024-07-22 21:54:43 +0530

    02 Problem, Chapter 2 - PS, find remainder when a number is divided by z.

---
+++ Chapter 2 - PS/02_problem2.py
@@ -0,0 +1,11 @@
+# 2. Write a python program to find remainder when a number is divided by z.
+
+a = 37
+
+b = 5
+
+print("Remainder when a is divided by b is", a % b)
+
+# Output:
+# The-Ultimate-Python-Course-CWH\Chapter 2 - PS> python .\02_problem2.py
+# Remainder when a is divided by b is 2
