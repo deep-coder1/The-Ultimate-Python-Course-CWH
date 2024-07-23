3c1cfbd1b6dce88c899a59782ec33c82a0e79106
deep-coder1 <ndktbd@gmail.com>
2024-07-23 21:33:05 +0530

    03 Problem, Chapter 2 - PS, Check the type of variable assigned using input () function

---
+++ Chapter 2 - PS/03_problem3.py
@@ -0,0 +1,9 @@
+# 3. Check the type of variable assigned using input () function.
+
+a = input("Enter the value of a: ")
+print(type(a))
+
+# Output:
+# The-Ultimate-Python-Course-CWH\Chapter 2 - PS> python .\03_problem3.py
+# Enter the value of a: 5
+# <class 'str'>
