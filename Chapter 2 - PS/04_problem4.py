ba218c280524a6d08276df13a99a11a3ce170972
deep-coder1 <ndktbd@gmail.com>
2024-07-24 08:15:37 +0530

    04 Problem, Chapter 2 - PS, Use comparison operator .

---
+++ Chapter 2 - PS/04_problem4.py
@@ -0,0 +1,17 @@
+# 4. Use comparison operator to find out whether ‘a’ given variable a is greater than 
+# ‘b’ or not. Take a = 34 and b = 80
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+print("a is greater than b is", a>b)
+
+# Output:
+# PS E:\Study\Python\The-Ultimate-Python-Course-CWH\Chapter 2 - PS> python .\04_problem4.py
+# Enter number 1: 5
+# Enter number 2: 8
+# a is greater than b is False
+# PS E:\Study\Python\The-Ultimate-Python-Course-CWH\Chapter 2 - PS> python .\04_problem4.py
+# Enter number 1: 8
+# Enter number 2: 4
+# a is greater than b is True
