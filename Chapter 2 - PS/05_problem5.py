ad2ec7a410ea99a8b01da03e46926399395cf807
deep-coder1 <ndktbd@gmail.com>
2024-07-25 22:30:08 +0530

    05 Problem, Chapter 2 - PS, find an average of two numbers entered by the user.

---
+++ Chapter 2 - PS/05_problem5.py
@@ -0,0 +1,13 @@
+# 5. Write a python program to find an average of two numbers entered by the user.
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+
+print("The average of these two number is", (a+b)/2)
+
+# Output:
+# PS E:\Study\Python\The-Ultimate-Python-Course-CWH\Chapter 2 - PS> python .\05_problem5.py
+# Enter number 1: 8
+# Enter number 2: 2
+# The average of these two number is 5.0
