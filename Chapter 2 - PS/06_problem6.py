ad80a78f2a8b06e26481a33732d96983f1f346c0
deep-coder1 <ndktbd@gmail.com>
2024-07-26 22:19:41 +0530

    06 Problem, Chapter 2 - PS, Calculate the square of a number entered by the user.

---
+++ Chapter 2 - PS/06_problem6.py
@@ -0,0 +1,14 @@
+# 6. Write a python program to calculate the square of a number entered by the user.
+
+a = int(input("Enter your number: ")) 
+
+
+print("The square of the number is", a**2)
+print("The square of the number is", a*a)
+# print("The square of the number is", a^2) # Incorrect for finding square of a number in Python
+
+# Output:
+# PS E:\Study\Python\The-Ultimate-Python-Course-CWH\Chapter 2 - PS> python .\06_problem6.py
+# Enter your number: 5
+# The square of the number is 25
+# The square of the number is 25
