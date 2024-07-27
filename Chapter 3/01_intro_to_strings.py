ec8be378b12f01d7df1586767255b0c3ea450abc
deep-coder1 <ndktbd@gmail.com>
2024-07-27 17:13:40 +0530

    Chapter 3, Intro to strings.

---
+++ Chapter 3/01_intro_to_strings.py
@@ -0,0 +1,11 @@
+name = "Deepak"
+
+nameshort = name[0:3] # start from index 0 all the way till 3 (excluding 3)
+print(nameshort)
+character1 = name[1]
+print(character1)
+
+# Output:
+# Python\The-Ultimate-Python-Course-CWH\Chapter 3> python .\01_intro_to_strings.py
+# Dee
+# e
