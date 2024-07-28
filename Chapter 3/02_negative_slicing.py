986f21986ca5e2b0ebe169785af59448e7ed4f1d
deep-coder1 <ndktbd@gmail.com>
2024-07-28 22:45:38 +0530

    Chapter 3, Negative slicing.

---
+++ Chapter 3/02_negative_slicing.py
@@ -0,0 +1,19 @@
+name = "Deepak"
+
+print(name[0:3])
+
+print(name[-4: -1])
+print(name[1:4])
+
+print(name[:4]) # is same as print(name[0:4])
+print(name[1:]) # is same as print(name[1:5])
+print(name[1:5])
+
+# Output:
+# Python\The-Ultimate-Python-Course-CWH\Chapter 3> python .\02_negative_slicing.py
+# Dee
+# epa
+# eep
+# Deep
+# eepak
+# eepa
