54632b0d7c45d768202bb7ae7f08742c538254d9
deep-coder1 <ndktbd@gmail.com>
2024-07-29 17:55:03 +0530

    Chapter 3, str functions.

---
+++ Chapter 3/03_str_functions.py
@@ -0,0 +1,12 @@
+name = "deepak"
+
+print(len(name))
+print(name.endswith("pak"))
+print(name.startswith("de"))
+print(name.capitalize())
+
+# Output:
+# Python\The-Ultimate-Python-Course-CWH\Chapter 3> python .\03_str_functions.py
+# 6
+# True
+# True
