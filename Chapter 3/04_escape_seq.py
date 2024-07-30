57de6152770b268db22ccc19d25f532c878f1f9b
deep-coder1 <ndktbd@gmail.com>
2024-07-30 18:16:17 +0530

    Chapter 3, Escape Seq.

---
+++ Chapter 3/04_escape_seq.py
@@ -0,0 +1,8 @@
+a = 'Deepak is a good boy\nbut not a bad \'boy\''
+
+
+print(a)
+
+# Python\The-Ultimate-Python-Course-CWH\Chapter 3> python .\04_escape_seq.py
+# Deepak is a good boy
+# but not a bad 'boy'
