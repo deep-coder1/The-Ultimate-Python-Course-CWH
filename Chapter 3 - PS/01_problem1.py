6932b8e6d7263907355977990d8df83988089c67
deep-coder1 <ndktbd@gmail.com>
2024-07-31 21:15:29 +0530

    01 Problem, Chapter 3 - PS, Program to display a user entered name followed by Good Afternoon using input () function.

---
+++ Chapter 3 - PS/01_problem1.py
@@ -0,0 +1,10 @@
+# 1. Write a python program to display a user entered name followed by Good 
+# Afternoon using input () function.
+
+name = input("Enter your name: ")
+
+print(f"Good Afternoon, {name} ") 
+
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 3 - PS> python .\01_problem1.py
+# Enter your name: Deepak
+# Good Afternoon, Deepak 
