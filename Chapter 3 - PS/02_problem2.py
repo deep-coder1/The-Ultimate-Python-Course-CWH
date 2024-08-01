c698e2ef92c856e1a1e4f021b57837fb8ee19648
deep-coder1 <ndktbd@gmail.com>
2024-08-01 17:40:24 +0530

    02 Problem, Chapter 3 - PS, Program to fill in a letter template given below with name and date.

---
+++ Chapter 3 - PS/02_problem2.py
@@ -0,0 +1,17 @@
+# 2. Write a program to fill in a letter template given below with name and date.
+# letter = ''' 
+# Dear <|Name|>,
+# You are selected!
+# <|Date|>
+# '''
+
+letter = '''Dear <|Name|>, 
+You are selected! 
+<|Date|> '''
+
+print(letter.replace("<|Name|>", "Deepak").replace("<|Date|", "30 August 2024"))
+
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 3 - PS> python .\02_problem2.py
+# Dear Deepak, 
+# You are selected! 
+# 30 August 2024> 
