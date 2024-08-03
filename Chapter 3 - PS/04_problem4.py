13b18a3afcaa26c0b9616c54358af03772b8e98f
deep-coder1 <ndktbd@gmail.com>
2024-08-03 21:21:38 +0530

    04 Problem Chapter 3, Replace the double space from problem 3 with single spaces.

---
+++ Chapter 3 - PS/04_problem4.py
@@ -0,0 +1,10 @@
+# 4. Replace the double space from problem 3 with single spaces.
+
+name = "Deepak is a good  boy and  "
+
+print(name.replace("  ", " "))
+print(name) # Strings are immutable which means that you cannot change them by running functions on them
+
+# Python\The-Ultimate-Python-Course-CWH\Chapter 3 - PS> python .\04_problem4.py
+# Deepak is a good boy and 
+# Deepak is a good  boy and  
