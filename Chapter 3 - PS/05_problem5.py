5bc12ad8e822452dc06ad149873f4eff90ecbf21
deep-coder1 <ndktbd@gmail.com>
2024-08-04 18:39:17 +0530

    05 Problem Chapter 3 - PS, Program to format the following letter using escape sequence

---
+++ Chapter 3 - PS/05_problem5.py
@@ -0,0 +1,12 @@
+# 5. Write a program to format the following letter using escape sequence 
+# characters.
+# letter = "Dear Deepak, this python course is nice. Thanks!"
+
+letter = "Dear Deepak,\n\tThis python course is nice.\nThanks!"
+
+print(letter)
+
+# PS E:\Study\Python\The-Ultimate-Python-Course-CWH\Chapter 3 - PS> python .\05_problem5.py
+# Dear Deepak,
+#         This python course is nice.
+# Thanks!
