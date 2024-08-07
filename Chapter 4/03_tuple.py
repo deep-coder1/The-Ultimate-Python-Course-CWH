25bde279e977736386f6e54652d0ed59b760006d
deep-coder1 <ndktbd@gmail.com>
2024-08-07 20:16:09 +0530

    Chapter 4, 03 _ tuple.py.

---
+++ Chapter 4/03_tuple.py
@@ -0,0 +1,8 @@
+a = (1,45,342,3424,False, "Rohan", "Shivam")
+print(a)
+print(type(a))
+
+# Output:
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 4> python .\03_tuple.py
+# (1, 45, 342, 3424, False, 'Rohan', 'Shivam')
+# <class 'tuple'>
