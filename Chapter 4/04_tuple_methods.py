f1ee09024cd7c725d6955d1105f8fd1568f20fbe
deep-coder1 <ndktbd@gmail.com>
2024-08-08 22:27:02 +0530

    Chapter 4, 04 _ tuple_methods.py.

---
+++ Chapter 4/04_tuple_methods.py
@@ -0,0 +1,16 @@
+a = (1,45,342,3424,False, 45, "Rohan", "Shivam")
+print(a) 
+
+no = a.count(45)
+print(no)
+
+i = a.index(3424)
+print(i)
+
+print(len(a))
+
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 4> python .\04_tuple_methods.py
+# (1, 45, 342, 3424, False, 45, 'Rohan', 'Shivam')
+# 2
+# 3
+# 8
