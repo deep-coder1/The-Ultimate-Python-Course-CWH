d35889337cb86da05e6d08eecbc9eb7489ca7262
deep-coder1 <ndktbd@gmail.com>
2024-08-12 20:37:57 +0530

    04 Problem Chapter 4 - PS, Program to sum a list with 4 numbers.

---
+++ Chapter 4 - PS/04_problem4.py
@@ -0,0 +1,9 @@
+# 4. Write a program to sum a list with 4 numbers.
+l = [3, 3, 5, 1]
+
+
+print(sum(l))
+
+# Output:
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 4 - PS> python .\04_problem4.py
+# 12
