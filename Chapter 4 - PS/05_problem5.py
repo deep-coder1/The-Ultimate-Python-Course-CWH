5417cbbe4a56dbec090a2dbb5c50922cbfbc5f9b
deep-coder1 <ndktbd@gmail.com>
2024-08-14 21:39:03 +0530

    05 Problem Chapter 4 - PS, Program to count the number of zeros in the following tuple:

---
+++ Chapter 4 - PS/05_problem5.py
@@ -0,0 +1,11 @@
+# 5. Write a program to count the number of zeros in the following tuple:
+# a = (7, 0, 8, 0, 0, 9)
+
+a = (7, 0, 8, 0, 0, 9)
+
+n = a.count(0)
+print(n)
+
+# Output:
+# /Python\The-Ultimate-Python-Course-CWH\Chapter 4 - PS> python .\05_problem5.py
+# 3
