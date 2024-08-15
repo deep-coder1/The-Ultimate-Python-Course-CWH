0c7139d5db135088e3507264f8b88c1af3868fef
deep-coder1 <ndktbd@gmail.com>
2024-08-15 21:30:06 +0530

    Chapter 5, 01 _ dict.py.

---
+++ Chapter 5/01_dict.py
@@ -0,0 +1,16 @@
+d = {} # Empty dictionary
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23
+}
+
+
+# print(marks, type(marks))
+print(marks["Harry"])
+
+# Output:
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 4> python .\01_list.py
+# Apple
+# Grapes
+# ['Orange', 5, 345.06]
