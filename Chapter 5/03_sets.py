180bbba5e3a7bee2e4e55cfad30ca7a1f77906a7
deep-coder1 <ndktbd@gmail.com>
2024-08-17 22:35:18 +0530

    Chapter 5, 03 _ sets.py.

---
+++ Chapter 5/03_sets.py
@@ -0,0 +1,9 @@
+e = set() # Dont use s = {} as it will create an empty dictionary
+s = {1, 5, 32, 54,5, 5, 5} 
+
+
+print(s)
+
+# Output:
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 5> python .\03_sets.py
+# {32, 1, 5, 54}
