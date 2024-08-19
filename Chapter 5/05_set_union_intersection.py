20185f4295be61ab4028c0bec6c083b5e4894cf6
deep-coder1 <ndktbd@gmail.com>
2024-08-19 21:11:45 +0530

    Chapter 5, 05_set_union_intersection.py.

---
+++ Chapter 5/05_set_union_intersection.py
@@ -0,0 +1,10 @@
+s1 = {1, 45, 6, 78}
+s2 = {7, 8, 1, 78}
+
+print(s1.union(s2))
+print(s1.intersection(s2))
+
+# Output:
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 5> python .\05_set_union_intersection.py
+# {1, 6, 7, 8, 45, 78}
+# {1, 78}
