26adcd6ef5d0356095c5dce2745169e91805d4b3
deep-coder1 <ndktbd@gmail.com>
2024-08-20 18:51:06 +0530

    01 Problem Chapter 5 - PS, Program to create a dictionary of Hindi words with values as their English # translation. Provide user with an option to look it up!

---
+++ Chapter 5 - PS/01_problem1.py
@@ -0,0 +1,17 @@
+# 1. Write a program to create a dictionary of Hindi words with values as their English
+# translation. Provide user with an option to look it up!
+
+words = {
+    "madad": "Help",
+    "kursi": "Chair",
+    "billi": "Cat"
+}
+
+word = input("Enter the word you want meaning of: ")
+
+print(words[word])
+
+# OutpuT:
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 5 - PS> python .\01_problem1.py
+# Enter the word you want meaning of: kursi
+# Chair
