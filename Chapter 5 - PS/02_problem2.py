6545c71aae3a22a5736f1d1f17b0264b907b3164
deep-coder1 <ndktbd@gmail.com>
2024-08-22 20:21:54 +0530

    02 Problem Chapter 5 - PS, Program to input eight numbers fr… …om the user and display all the unique # numbers (once).

--- Chapter 5 - PS/02_problem2.py
+++ Chapter 5 - PS/02_problem2.py
@@ -20,3 +20,15 @@ n = input("Enter number: ")
 s.add(int(n))  
 
 print(s)
+
+# Output:
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 5 - PS> python .\02_problem2.py
+# Enter number: 8
+# Enter number: 5
+# Enter number: 4
+# Enter number: 9
+# Enter number: 12
+# Enter number: 47
+# Enter number: 52
+# Enter number: 13
+# {4, 5, 8, 9, 12, 13, 47, 52}
