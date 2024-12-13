64a929172917054f0206a17153529baf51c98ce1
deep-coder1 <ndktbd@gmail.com>
2024-12-13 21:12:53 +0530

    Chapter 6,working on 01_conditionals.py

--- Chapter 5 - PS/09_problem9.py
+++ Chapter 5 - PS/09_problem9.py
@@ -6,3 +6,8 @@ s = {8, 7, 12, "Harry", [1,2]}
 s[4][0] = 9
 
 # Output:
+# \Python\The-Ultimate-Python-Course-CWH\Chapter 5 - PS> python .\09_problem9.py
+# Traceback (most recent call last):
+#   File "E:\Study\Python\The-Ultimate-Python-Course-CWH\Chapter 5 - PS\09_problem9.py", line 4, in <module>
+#     s = {8, 7, 12, "Harry", [1,2]}
+# TypeError: unhashable type: 'list'
---
+++ Chapter 6/01_conditionals.py
@@ -0,0 +1,3 @@
+a = int(input("Enter your age: "))
+
+# If else statement
