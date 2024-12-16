844dd7de027be93387d42c39e66295a6daf39181
deep-coder1 <ndktbd@gmail.com>
2024-12-16 19:43:51 +0530

    Chapter 6,working 3 on 01_conditionals.py

--- Chapter 6/01_conditionals.py
+++ Chapter 6/01_conditionals.py
@@ -10,3 +10,15 @@ else:
 
 
 print("End of Program")
+
+# O/P:
+# /Chapter 6/01_conditionals.py"
+# Enter your age: 25
+# You are above the age of consent
+# Good for you
+# End of Program
+
+# Chapter 6/01_conditionals.py"
+# Enter your age: 16
+# You are below the age of consent
+# End of Program
