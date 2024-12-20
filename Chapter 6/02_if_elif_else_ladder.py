2de3475f558373457a473693c01adff6fa56b556
deep-coder1 <ndktbd@gmail.com>
2024-12-20 12:31:13 +0530

    Chapter 6,working 3 on 02_if_elif_else_ladder.py

--- Chapter 6/02_if_elif_else_ladder.py
+++ Chapter 6/02_if_elif_else_ladder.py
@@ -4,3 +4,6 @@ a = int(input("Enter your age: "))
 if(a>=18):
     print("You are above the age of consent")
     print("Good for you")
+    
+elif(a<0):
+    print("You are entering an invalid negative age")
