4a07d39f6277444928fb4c3afadf421045b314aa
deep-coder1 <ndktbd@gmail.com>
2025-03-11 20:12:52 +0530

    01 Problem 1.6 Chapter 6 - PS, Find greatest of four numbers entered by the users

--- Chapter 6 - PS/01_problem1.py
+++ Chapter 6 - PS/01_problem1.py
@@ -3,4 +3,4 @@ a2 = int(input("Enter number 2: "))
 a3 = int(input("Enter number 3: "))
 a4 = int(input("Enter number 4: "))
 if(a1>a2 and a1>a3 and a1>a4):
-    
+    print("Greatest number is a1:", a1)
