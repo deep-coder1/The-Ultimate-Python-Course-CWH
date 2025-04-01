684e6eec77539b3ce65b69c476327b09dbec7de6
deep-coder1 <ndktbd@gmail.com>
2025-04-01 20:13:30 +0530

    02 Problem2 1.6Chapter 6 - PS working.

--- Chapter 6 - PS/02_problem2.py
+++ Chapter 6 - PS/02_problem2.py
@@ -9,3 +9,6 @@ marks3 = int(input("Enter Marks 3: "))
 # Check for total percentage
 total_percentage = (100*(marks1 + marks2 + marks3))/300
 
+if(total_percentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+
+
