1206fafe94583bc5299dfd466866cbda9c421230
deep-coder1 <ndktbd@gmail.com>
2025-12-04 16:30:43 +0530

    01 Problem 1.0 Chapter 3 - PS working.

--- Chapter 3 - PS/01_problem1.py
+++ Chapter 3 - PS/01_problem1.py
@@ -5,6 +5,10 @@ name = input("Enter your name: ")
 
 print(f"Good Afternoon, {name} ") 
 
+# Output 1 :
 # \Python\The-Ultimate-Python-Course-CWH\Chapter 3 - PS> python .\01_problem1.py
 # Enter your name: Deepak
 # Good Afternoon, Deepak 
+
+# Output 2:
+# /Python/The-Ultimate-Python-Course-CWH/Chapter 3 - PS/01_problem1.py"
