9b616f584cfcb5be395b40ed27e0da0185528814
deep-coder1 <ndktbd@gmail.com>
2025-12-12 14:21:42 +0530

    01 Problem 1.7 Chapter 3 - PS working.

--- Chapter 3 - PS/01_problem1.py
+++ Chapter 3 - PS/01_problem1.py
@@ -13,4 +13,4 @@ print(f"Good Afternoon, {name} ")
 # Output 2:
 # /Python/The-Ultimate-Python-Course-CWH/Chapter 3 - PS/01_problem1.py"
 # Enter your name: Sandeep
-# Good Afternoon, 
+# Good Afternoon, Sandeep
