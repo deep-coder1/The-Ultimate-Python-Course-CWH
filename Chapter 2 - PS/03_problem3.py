90ed1f446285bd3856978bbd0ff3efdb14442478
deep-coder1 <ndktbd@gmail.com>
2025-12-13 20:59:22 +0530

    03 Problem 1.0 Chapter 2 - PS working.

--- Chapter 2 - PS/03_problem3.py
+++ Chapter 2 - PS/03_problem3.py
@@ -3,7 +3,9 @@
 a = input("Enter the value of a: ")
 print(type(a))
 
-# Output:
+# Output 1:
 # The-Ultimate-Python-Course-CWH\Chapter 2 - PS> python .\03_problem3.py
 # Enter the value of a: 5
 # <class 'str'>
+
+# Output 2:
