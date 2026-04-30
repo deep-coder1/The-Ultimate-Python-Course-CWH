3d11cfcc1986375e66f031f0237afd1fa4a86628
deep-coder1 <ndktbd@gmail.com>
2026-05-01 01:48:12 +0530

    01 Problem 4.1.59 Chapter 5 - PS working.

--- Chapter 4 - PS/01_problem1.py
+++ Chapter 4 - PS/01_problem1.py
@@ -50,4 +50,4 @@ print(fruits)
 # Enter Fruit name: Lime
 # Enter Fruit name: Strawberry
 # Enter Fruit name: Coconut
-# ['Jackfruit', 'Cherry', 'Kiwi', 'Pear', 'Lime', 'Strawberry'
+# ['Jackfruit', 'Cherry', 'Kiwi', 'Pear', 'Lime', 'Strawberry', 
